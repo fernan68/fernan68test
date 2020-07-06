57812f918dcd218e7722d976ef9e9bee76b867f7
fernan68 <Cesar.FernandezDelgado.ext@imec.be>
2020-07-06 18:27:27 +0200

    first commit

---
+++ spec/my-package-fernan68-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyPackageFernan68TestView from '../lib/my-package-fernan68-test-view';
+
+describe('MyPackageFernan68TestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
